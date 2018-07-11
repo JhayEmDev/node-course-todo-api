e6c9a42b3e157d0733f0337af4ac88e3359f8306
JhayEm <jhay.em.dev@gmail.com>
2018-07-11 14:56:36 +0800

    Added tests

--- server/server.js
+++ server/server.js
@@ -9,7 +9,6 @@ const app = express();
 app.use(bodyParser.json());
 
 app.post('/todos', async (req, res) => {
-  console.log(req.body);
   const todo = new Todo({
     text: req.body.text
   });
@@ -19,7 +18,6 @@ app.post('/todos', async (req, res) => {
     doc = await todo.save();
     res.send(doc);
   } catch (e) {
-    console.log('Error saving todo from post man');
     res.status(400).send(e);
   }
 });
@@ -27,3 +25,5 @@ app.post('/todos', async (req, res) => {
 app.listen(3000, () => {
   console.log(`Started server on port 3000`);
 });
+
+module.exports = {app};
